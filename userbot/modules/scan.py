750b8a631b32796cf096552f52c6041e72697bac
gr8ixma <87972651+gr8ixma@users.noreply.github.com>
2021-10-10 04:21:05 +0800

    Update and rename sangmata.py to scan.py

--- userbot/modules/scan.py
+++ userbot/modules/scan.py
@@ -9,16 +9,16 @@ async def lastname(steal):
     if steal.fwd_from:
         return
     if not steal.reply_to_msg_id:
-        await steal.edit("```Mohon Balas Ke Pesan Pengguna.```")
+        await steal.edit("```Reply chat dia.```")
         return
     message = await steal.get_reply_message()
     chat = "@SangMataInfo_bot"
     user_id = message.sender.id
     id = f"/search_id {user_id}"
     if message.sender.bot:
-        await steal.edit("```Balas Ke Pesan Pengguna Yang Sebenarnya.```")
+        await steal.edit("```Reply chat dia untuk scan.```")
         return
-    await steal.edit("```Mengambil Informasi Pengguna Tersebut, Mohon Menunggu..```")
+    await steal.edit("```Tunggu sat , minum red bull dulu```")
     try:
         async with bot.conversation(chat) as conv:
             try:
@@ -27,7 +27,7 @@ async def lastname(steal):
                 response = await conv.get_response()
             except YouBlockedUserError:
                 await steal.reply(
-                    "```Mohon Unblock @sangmatainfo_bot Dan Coba Lagi```"
+                    "```Unblock @sangmatainfo_bot n try lagi```"
                 )
                 return
             if r.text.startswith("Name"):
@@ -40,7 +40,7 @@ async def lastname(steal):
             if response.text.startswith("No records") or r.text.startswith(
                 "No records"
             ):
-                await steal.edit("```Saya Tidak Menemukan Informasi Pengguna Ini, Pengguna Ini Belum Pernah Mengganti Nama Sebelumnya```")
+                await steal.edit("```Takde record la, maybe member tak baru create acc```")
                 await steal.client.delete_messages(
                     conv.chat_id, [msg.id, r.id, response.id]
                 )
@@ -52,11 +52,11 @@ async def lastname(steal):
                 conv.chat_id, [msg.id, r.id, response.id, respond.id]
             )
     except TimeoutError:
-        return await steal.edit("`Saya Sedang Sakit Mohon Maaf`")
+        return await steal.edit("`I'm sick n broken`")
 
 
 CMD_HELP.update({
     "sangmata":
         "`.sa`\
-          \nUsage: Mendapatkan Riwayat Nama Pengguna."
+          \nUsage: Tengah attack hihi."
 })
